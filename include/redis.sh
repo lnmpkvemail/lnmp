1c47043ce9892edbe5db6e266288c02fda092964
licess <licess@gmail.com>
2015-10-28 15:46:41 +0800

    remove old redis support

--- include/redis.sh
+++ include/redis.sh
@@ -2,26 +2,8 @@
 
 Install_Redis()
 {
-    ver="1"
-    echo "Which version do you want to install:"
-    echo "Install Redis 3.0.1   Stable Version please type: 1"
-    echo "Install Redis 2.8.20  Old Version please type: 2"
-    read -p "Enter 1 or 2 (Default Stable version): " ver
-    if [ "${ver}" = "" ]; then
-        ver="1"
-    fi
-
-    if [ "${ver}" = "1" ]; then
-        echo "You will install Redis 3.0.1   Stable Version"
-    elif [ "${ver}" = "2" ]; then
-        echo "You will install Redis 2.8.20  Old Version"
-    else
-        echo "Input error,please input 1 or 2 !"
-        echo "Please Rerun $0"
-        exit 1
-    fi
-
     echo "====== Installing Redis ======"
+    echo "Install ${Redis_Stable_Ver} Stable Version..."
     Press_Install
 
     sed -i '/redis.so/d' /usr/local/php/etc/php.ini
@@ -32,13 +14,8 @@ Install_Redis()
     fi
 
     cd ${cur_dir}/src
-    if [ "${ver}" = "1" ]; then
-        Download_Files http://download.redis.io/releases/${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}.tar.gz
-        Tar_Cd ${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}
-    else
-        Download_Files http://download.redis.io/releases/${Redis_Old_Ver}.tar.gz ${Redis_Old_Ver}.tar.gz
-        Tar_Cd ${Redis_Old_Ver}.tar.gz ${Redis_Old_Ver}
-    fi
+    Download_Files http://download.redis.io/releases/${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}.tar.gz
+    Tar_Cd ${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}
 
     if [ "${Is_64bit}" = "y" ] ; then
         make PREFIX=/usr/local/redis install
@@ -48,7 +25,7 @@ Install_Redis()
     mkdir -p /usr/local/redis/etc/
     \cp redis.conf  /usr/local/redis/etc/
     sed -i 's/daemonize no/daemonize yes/g' /usr/local/redis/etc/redis.conf
-    sed -i 's/# bind 127.0.0.1/bind 127.0.0.1/g' /usr/local/redis/etc/redis.conf
+    sed -i 's/^# bind 127.0.0.1/bind 127.0.0.1/g' /usr/local/redis/etc/redis.conf
     cd ../
 
     if [ -s /sbin/iptables ]; then
