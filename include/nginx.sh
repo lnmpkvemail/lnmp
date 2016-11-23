98e8927cddc579edb2755e8c27d2a5e47603a44a
licess <licess@gmail.com>
2016-11-23 11:24:39 +0800

    add more secruity options

--- include/nginx.sh
+++ include/nginx.sh
@@ -71,11 +71,14 @@ Install_Nginx()
     fi
 
     if [ "${Stack}" = "lnmp" ]; then
-    cat >${Default_Website_Dir}/.user.ini<<EOF
+        cat >${Default_Website_Dir}/.user.ini<<EOF
 open_basedir=${Default_Website_Dir}:/tmp/:/proc/
 EOF
-    chmod 644 ${Default_Website_Dir}/.user.ini
-    chattr +i ${Default_Website_Dir}/.user.ini
+        chmod 644 ${Default_Website_Dir}/.user.ini
+        chattr +i ${Default_Website_Dir}/.user.ini
+        cat >>/usr/local/nginx/conf/fastcgi.conf<<EOF
+fastcgi_param PHP_ADMIN_VALUE "open_basedir=$document_root/:/tmp/:/proc/";
+EOF
     fi
 
     \cp init.d/init.d.nginx /etc/init.d/nginx
