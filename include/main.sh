740f9ee68be9d5d8a5ab208b9c45909521b733a9
licess <licess@gmail.com>
2017-02-12 15:08:00 +0800

    add get distro version use python * test *

--- include/main.sh
+++ include/main.sh
@@ -279,8 +279,15 @@ Install_LSB()
 
 Get_Dist_Version()
 {
-    Install_LSB
-    eval ${DISTRO}_Version=`lsb_release -rs`
+    if [ -s /usr/bin/python3 ]; then
+        eval ${DISTRO}_Version=`/usr/bin/python3 -c 'import platform; print(platform.linux_distribution()[1])'`
+    elif [ -s /usr/bin/python2 ]; then
+        eval ${DISTRO}_Version=`/usr/bin/python2 -c 'import platform; print platform.linux_distribution()[1]'`
+    fi
+    if [ $? -ne 0 ]; then
+        Install_LSB
+        eval ${DISTRO}_Version=`lsb_release -rs`
+    fi
     eval echo "${DISTRO} \${${DISTRO}_Version}"
 }
 
