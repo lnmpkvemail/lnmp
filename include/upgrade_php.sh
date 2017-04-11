1c0494903b071d8384dd782c90e2485c5221d825
licess <licess@gmail.com>
2017-04-11 15:59:35 +0800

    lnmp manager add multiplephp status manage support,fix php upgrade init.d file backup bug

--- include/upgrade_php.sh
+++ include/upgrade_php.sh
@@ -60,8 +60,8 @@ Start_Upgrade_PHP()
     lnmp stop
 
     if [ "${Stack}" = "lnmp" ]; then
-        mv /etc/init.d/php-fpm /usr/local/oldphp${Upgrade_Date}/init.d.php-fpm.bak.${Upgrade_Date}
         mv /usr/local/php /usr/local/oldphp${Upgrade_Date}
+        mv /etc/init.d/php-fpm /usr/local/oldphp${Upgrade_Date}/init.d.php-fpm.bak.${Upgrade_Date}
     else
         if echo "${Cur_PHP_Version}" | grep -Eqi '^7.';then
             mv /usr/local/apache/modules/libphp7.so /usr/local/apache/modules/libphp7.so.bak.${Upgrade_Date}
