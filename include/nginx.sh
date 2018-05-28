73262b48dce938716b41ace665eeffe35906d06f
licess <licess@gmail.com>
2018-05-28 09:54:59 +0800

    fix nginx bug under gcc8

--- include/nginx.sh
+++ include/nginx.sh
@@ -64,6 +64,9 @@ Install_Nginx()
     if [[ "${DISTRO}" = "Fedora" && "${Fedora_Version}" = "28" ]]; then
         patch -p1 < ${cur_dir}/src/patch/nginx-libxcrypt.patch
     fi
+    if gcc -dumpversion|grep -q "^[8]"; then
+        patch -p1 < ${cur_dir}/src/patch/nginx-gcc8.patch
+    fi
     if echo ${Nginx_Ver} | grep -Eqi 'nginx-[0-1].[5-8].[0-9]' || echo ${Nginx_Ver} | grep -Eqi 'nginx-1.9.[1-4]$'; then
         ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module ${Nginx_With_Openssl} ${Nginx_Module_Lua} ${NginxMAOpt} ${Nginx_Modules_Options}
     else
--- include/upgrade_nginx.sh
+++ include/upgrade_nginx.sh
@@ -51,6 +51,9 @@ Upgrade_Nginx()
     if [[ "${DISTRO}" = "Fedora" && "${Fedora_Version}" = "28" ]]; then
         patch -p1 < ${cur_dir}/src/patch/nginx-libxcrypt.patch
     fi
+    if gcc -dumpversion|grep -q "^[8]"; then
+        patch -p1 < ${cur_dir}/src/patch/nginx-gcc8.patch
+    fi
     if echo ${Nginx_Version} | grep -Eqi '^[0-1].[5-8].[0-9]' || echo ${Nginx_Version} | grep -Eqi '^1.9.[1-4]$'; then
         ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module ${Nginx_With_Openssl} ${Nginx_Module_Lua} ${NginxMAOpt} ${Nginx_Modules_Options}
     else
