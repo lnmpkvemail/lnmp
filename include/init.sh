c4dd90833ff7b5102bfbb593844bc003e6d325ae
licess <licess@gmail.com>
2018-08-24 20:54:06 +0800

    improve

--- include/init.sh
+++ include/init.sh
@@ -348,7 +348,7 @@ Install_Mhash()
 
 Install_Freetype()
 {
-    if [[ "${DISTRO}" = "Ubuntu" && "${Ubuntu_Version}" = "18.04" ]]; then
+    if [[ "${DISTRO}" = "Ubuntu" && "${Ubuntu_Version}" = "18.04" ]] || grep -Eqi "Mint 19" /etc/issue || grep -Eqi "Deepin GNU/Linux 15.7" /etc/issue; then
         Download_Files ${Download_Mirror}/lib/freetype/${Freetype_New_Ver}.tar.bz2 ${Freetype_New_Ver}.tar.bz2
         Echo_Blue "[+] Installing ${Freetype_New_Ver}"
         Tarj_Cd ${Freetype_New_Ver}.tar.bz2 ${Freetype_New_Ver}
