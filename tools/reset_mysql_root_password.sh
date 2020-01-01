bef4cadbc7c3435bcbd0d5ac5d013daf90e1d0c4
licess <licess@gmail.com>
2020-01-01 21:08:49 +0800

    improve mysql/mariadb root password reset tool

--- tools/reset_mysql_root_password.sh
+++ tools/reset_mysql_root_password.sh
@@ -45,7 +45,7 @@ echo "Starting ${DB_Name} with skip grant tables"
 /usr/local/${DB_Name}/bin/mysqld_safe --skip-grant-tables >/dev/null 2>&1 &
 sleep 5
 echo "update ${DB_Name} root password..."
-if echo "${DB_Ver}" | grep -Eqi '^8.0.|^5.7.|^10.2.'; then
+lif echo "${DB_Ver}" | grep -Eqi '^8.0.|^5.7.|^10.[234].'; then
     /usr/local/${DB_Name}/bin/mysql -u root << EOF
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_Root_Password}';
@@ -58,7 +58,11 @@ fi
 
 if [ $? -eq 0 ]; then
     echo "Password reset succesfully. Now killing mysqld softly"
-    killall mysqld
+    if command -v killall >/dev/null 2>&1; then
+        killall mysqld
+    else
+        kill `pidof mysqld`
+    fi
     sleep 5
     echo "Restarting the actual ${DB_Name} service"
     /etc/init.d/${DB_Name} start
