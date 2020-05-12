77e92eac5e1240df0a00e739eefbe21f173fb5f5
licess <licess@gmail.com>
2020-05-12 20:26:10 +0800

    use old stable redis when gcc version is below 5.x

--- include/redis.sh
+++ include/redis.sh
@@ -17,6 +17,9 @@ Install_Redis()
     if [ -s /usr/local/redis/bin/redis-server ]; then
         echo "Redis server already exists."
     else
+        if gcc -dumpversion|grep -q "^[34]."; then
+            Redis_Stable_Ver='redis-5.0.9'
+        fi
         Download_Files http://download.redis.io/releases/${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}.tar.gz
         Tar_Cd ${Redis_Stable_Ver}.tar.gz ${Redis_Stable_Ver}
 
