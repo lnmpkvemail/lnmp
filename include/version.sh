1f03c55e1b19b083d520c9e533c0ae8bb05cac6a
licess <licess@gmail.com>
2020-05-15 11:40:01 +0800

    update php version

--- include/version.sh
+++ include/version.sh
@@ -58,11 +58,11 @@ elif [ "${PHPSelect}" = "6" ]; then
 elif [ "${PHPSelect}" = "7" ]; then
     Php_Ver='php-7.1.33'
 elif [ "${PHPSelect}" = "8" ]; then
-    Php_Ver='php-7.2.30'
+    Php_Ver='php-7.2.31'
 elif [ "${PHPSelect}" = "9" ]; then
-    Php_Ver='php-7.3.17'
+    Php_Ver='php-7.3.18'
 elif [ "${PHPSelect}" = "10" ]; then
-    Php_Ver='php-7.4.5'
+    Php_Ver='php-7.4.6'
 fi
 if [[ "${PHPSelect}" =~ ^[123]$ ]]; then
     PhpMyAdmin_Ver='phpMyAdmin-4.0.10.20-all-languages'
