4bbf661095b1fcd685a51d8d2d2f88c7d91c7a58
licess <licess@gmail.com>
2020-07-18 10:19:13 +0800

    imporve

--- include/main.sh
+++ include/main.sh
@@ -653,9 +653,15 @@ Check_Mirror()
 
 Check_CMPT()
 {
-    if [[ "${DBSelect}" = "5" || "${PHPSelect}" == "10" ]]; then
+    if [[ "${DBSelect}" = "5" ]]; then
         if echo "${Ubuntu_Version}" | grep -Eqi "^1[0-7]\." || echo "${Debian_Version}" | grep -Eqi "^[4-8]" || echo "${Raspbian_Version}" | grep -Eqi "^[4-8]" || echo "${CentOS_Version}" | grep -Eqi "^[4-7]"  || echo "${RHEL_Version}" | grep -Eqi "^[4-7]" || echo "${Fedora_Version}" | grep -Eqi "^2[0-3]"; then
-            Echo_Red "PHP 7.4 or MySQL 8.0 please use latest linux distributions!"
+            Echo_Red "MySQL 8.0 please use latest linux distributions!"
+            exit 1
+        fi
+    fi
+    if [[ "${PHPSelect}" == "10" ]]; then
+        if echo "${Ubuntu_Version}" | grep -Eqi "^1[0-7]\." || echo "${Debian_Version}" | grep -Eqi "^[4-8]" || echo "${Raspbian_Version}" | grep -Eqi "^[4-8]" || echo "${CentOS_Version}" | grep -Eqi "^[4-6]"  || echo "${RHEL_Version}" | grep -Eqi "^[4-6]" || echo "${Fedora_Version}" | grep -Eqi "^2[0-3]"; then
+            Echo_Red "PHP 7.4 please use latest linux distributions!"
             exit 1
         fi
     fi
