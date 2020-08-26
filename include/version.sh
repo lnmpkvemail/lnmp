dee6f67f552eb2e8b37573ab4e5bf41f4c7991cd
licess <licess@gmail.com>
2020-08-26 11:03:54 +0800

    update apache version

--- include/version.sh
+++ include/version.sh
@@ -77,7 +77,7 @@ APR_Util_Ver='apr-util-1.6.1'
 if [ "${ApacheSelect}" = "1" ]; then
     Apache_Ver='httpd-2.2.34'
 elif [ "${ApacheSelect}" = "2" ]; then
-    Apache_Ver='httpd-2.4.43'
+    Apache_Ver='httpd-2.4.46'
 fi
 
 Pureftpd_Ver='pure-ftpd-1.0.49'
