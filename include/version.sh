cc999bb261637811d284ad02bd02f6f58ae5abec
licess <licess@gmail.com>
2021-05-13 14:04:10 +0800

    update version

--- include/version.sh
+++ include/version.sh
@@ -85,10 +85,10 @@ fi
 Pureftpd_Ver='pure-ftpd-1.0.49'
 
 XCache_Ver='xcache-3.2.0'
-ImageMagick_Ver='ImageMagick-7.0.10-8'
+ImageMagick_Ver='ImageMagick-7.0.11-12'
 Imagick_Ver='imagick-3.4.4'
 ZendOpcache_Ver='zendopcache-7.0.5'
-Redis_Stable_Ver='redis-6.2.2'
+Redis_Stable_Ver='redis-6.2.3'
 PHPRedis_Ver='redis-5.3.4'
 Memcached_Ver='memcached-1.6.7'
 Libmemcached_Ver='libmemcached-1.0.18'
