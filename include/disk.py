1230d6d84affded50ba09d1e297f80780e54ea08
licess <licess@gmail.com>
2022-01-29 12:13:06 +0800

    improve get free disk space script

--- include/disk.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import os
-
-dir = os.path.dirname(__file__)
-disk = os.statvfs(dir)
-Avail = disk.f_bavail * disk.f_bsize / (1024*1024*1024)
-print(int(Avail))
--- include/init.sh
+++ include/init.sh
@@ -848,20 +848,9 @@ Remove_Error_Libcurl()
 
 Add_Swap()
 {
-    if ! command -v python >/dev/null 2>&1 && ! command -v python2 >/dev/null 2>&1 && ! command -v python3 >/dev/null 2>&1; then
-        if [ "$PM" = "yum" ]; then
-            yum -y install python3
-        elif [ "$PM" = "apt" ]; then
-            apt-get --no-install-recommends install -y python3
-        fi
-    fi
-    if command -v python >/dev/null 2>&1; then
-        Disk_Avail=$(python ${cur_dir}/include/disk.py)
-    elif command -v python3 >/dev/null 2>&1; then
-        Disk_Avail=$(python3 ${cur_dir}/include/disk.py)
-    elif command -v python2 >/dev/null 2>&1; then
-        Disk_Avail=$(python2 ${cur_dir}/include/disk.py)
-    fi
+
+    Disk_Avail=$(($(df -m /var | tail -1 | awk '{print $4}' | sed s/[[:space:]]//g)/1024))
+
     DD_Count='1024'
     if [[ "${MemTotal}" -lt 1024 ]]; then
         DD_Count='1024'
