99a1295e2520bcca7bb018f770e2f5a6c2da26a2
licess <licess@gmail.com>
2022-03-08 09:21:47 +0800

    improve ioncube loader install scripts

--- include/ionCube.sh
+++ include/ionCube.sh
@@ -7,65 +7,53 @@ Install_ionCube()
 
     rm -f ${PHP_Path}/conf.d/001-ioncube.ini
     Addons_Get_PHP_Ext_Dir
-    if echo "${Cur_PHP_Version}" | grep -Eqi '^5.2.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_5.2.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^5.3.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_5.3.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^5.4.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_5.4.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^5.5.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_5.5.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^5.6.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_5.6.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^7.0.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_7.0.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^7.1.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_7.1.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^7.2.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_7.2.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^7.3.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_7.3.so"
-    elif echo "${Cur_PHP_Version}" | grep -Eqi '^7.4.'; then
-       zend_ext="/usr/local/ioncube/ioncube_loader_lin_7.4.so"
+    PHP_Short_Ver="$(echo ${Cur_PHP_Version} | cut -d. -f1-2)"
+    if echo ${zend_ext_dir} | grep -Eqi "non-zts"; then
+        zend_ext="ioncube_loader_lin_${PHP_Short_Ver}.so"
     else
-        Echo_Red "Do not support current PHP version or PHP error!"
-        exit 1
+        zend_ext="ioncube_loader_lin_${PHP_Short_Ver}_ts.so"
     fi
 
-    rm -rf /usr/local/ioncube
     cd ${cur_dir}/src
     rm -rf ioncube
     rm -rf ioncube_loaders_lin_x8*.tar.gz
+    [[ "${ARCH}" = "i386" ]] && ARCH='x86'
+    [[ "${ARCH}" = "x86_64" ]] && ARCH='x86-64'
+    [[ "${ARCH}" = "armhf" ]] && ARCH='armv7l'
     if grep -Eqi "xcache.so" ${PHP_Path}/conf.d/006-xcache.ini; then
-        if [ "${Is_64bit}" = "y" ] ; then
-            Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
-            tar zxf ioncube_loaders_lin_x86-64.tar.gz
+        if echo "${ARCH}" | grep -Eqi "x86*"; then
+            Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_${ARCH}.tar.gz ioncube_loaders_lin_${ARCH}.tar.gz
         else
-            Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
-            tar zxf ioncube_loaders_lin_x86.tar.gz
+            Echo_Red "Unsupported architecture with xcache! You can try uninstall xcache and reinstall ioncube loader."
+            exit 1
         fi
     else
-        if [ "${Is_64bit}" = "y" ] ; then
-            Download_Files https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
-            tar zxf ioncube_loaders_lin_x86-64.tar.gz
+        if echo "${ARCH}" | grep -Eqi "x86*|armv7l|aarch64"; then
+            Download_Files https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_${ARCH}.tar.gz ioncube_loaders_lin_${ARCH}.tar.gz
         else
-            Download_Files https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
-            tar zxf ioncube_loaders_lin_x86.tar.gz
+            Echo_Red "Unsupported architecture!"
+            exit 1
         fi
     fi
-    mv ioncube /usr/local/
+    tar zxf ioncube_loaders_lin_${ARCH}.tar.gz
+    if [ ! -d "/usr/local/ioncube" ]; then
+        mkdir -p /usr/local/ioncube
+    fi
+    if [ -s "ioncube/${zend_ext}" ]; then
+        \cp "ioncube/${zend_ext}" /usr/local/ioncube/
+    else
+        Echo_Red "ioncube does not support the current PHP version!"
+        exit 1
+    fi
 
     echo "Writing ionCube Loader to configure files..."
     cat >${PHP_Path}/conf.d/001-ioncube.ini<<EOF
 [ionCube Loader]
-zend_extension="${zend_ext}"
+zend_extension="/usr/local/ioncube/${zend_ext}"
 ;ioncubeend
 EOF
 
-    if [ -s "${zend_ext}" ]; then
-        if echo ${zend_ext_dir} | grep -vEqi "non-zts"; then
-            sed -i 's/.so/_ts.so/g' ${PHP_Path}/conf.d/001-ioncube.ini
-        fi
+    if [ -s "/usr/local/ioncube/${zend_ext}" ]; then
         Restart_PHP
         Echo_Green "====== ionCube install completed ======"
         Echo_Green "ionCube installed successfully, enjoy it!"
