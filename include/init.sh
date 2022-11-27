609afe3af94a7bf873e919f2ffb105bddd4bbe02
licess <licess@gmail.com>
2022-11-27 16:34:51 +0800

    improve under Anolis OS 7

--- include/init.sh
+++ include/init.sh
@@ -368,7 +368,7 @@ CentOS_Dependent()
         dnf install libarchive -y
     fi
 
-    if echo "${CentOS_Version}" | grep -Eqi "^7" || echo "${RHEL_Version}" | grep -Eqi "^7"  || echo "${Aliyun_Version}" | grep -Eqi "^2" || echo "${Alibaba_Version}" | grep -Eqi "^2" || echo "${Oracle_Version}" | grep -Eqi "^7"; then
+    if echo "${CentOS_Version}" | grep -Eqi "^7" || echo "${RHEL_Version}" | grep -Eqi "^7"  || echo "${Aliyun_Version}" | grep -Eqi "^2" || echo "${Alibaba_Version}" | grep -Eqi "^2" || echo "${Oracle_Version}" | grep -Eqi "^7" || echo "${Anolis_Version}" | grep -Eqi "^7"; then
         if [ "${DISTRO}" = "Oracle" ]; then
             yum -y install oracle-epel-release
             yum -y --enablerepo=*EPEL* install oniguruma-devel
